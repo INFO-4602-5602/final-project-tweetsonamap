c32d35eb16276cc6779b1cc4b77828b102304166
Melissa <melissa.j.bica@gmail.com>
2017-04-27 16:17:50 -0600

    working timeline with brushing

--- docs/js/timeline.js
+++ docs/js/timeline.js
@@ -2,11 +2,10 @@
 
 module.exports = function(config){
 
-
     this.createTimeline = function(){
     	console.log("Starting the timeline vis")
 
-        var margin = {top: 10, right: 20, bottom: 70, left: 60}; // Margin around visualization
+        var margin = {top: 10, right: 10, bottom: 70, left: 60}; // Margin around visualization, including space for labels
         var width = d3.select('#timeline').node().getBoundingClientRect().width - margin.left - margin.right; // Width of our visualization
         var height = 200 - margin.top - margin.bottom; // Height of our visualization
         // var transDur = 100; // Transition time in ms
@@ -14,6 +13,9 @@ module.exports = function(config){
         var dataset = "http://epic-analytics.cs.colorado.edu:9000/jennings/infovis/matthew_tweets_per_day.csv"
         
         var parseDate = d3.time.format("%Y-%m-%d").parse;
+        var formatDate = d3.time.format("%a %b %d, %Y");
+
+
 
         d3.csv(dataset, function(csvData){
             var data = csvData;
@@ -29,9 +31,16 @@ module.exports = function(config){
                             .rangeRoundBands([0, width], .05)
                             .domain(data.map(function(d) { return d.date; }));
 
+            var xScaleIdx = d3.scale.ordinal()
+                            .rangeRoundBands([0, width], .05)
+                            .domain(data.map(function(d) { return d.idx; }));
+            
             var yScale = d3.scale.linear()
                             .range([height, 0])
                             .domain([0, d3.max(data, function(d) { return parseFloat(d.count); })+1]);
+
+            var brushYearStart = d3.min(data, function(d) { return d.idx})
+            var brushYearEnd   = d3.max(data, function(d) { return d.idx})
 
             // Create an SVG element to contain our visualization.
             var svg = d3.select("#timeline").append("svg")
@@ -102,7 +111,7 @@ module.exports = function(config){
                 .attr('class', 'd3-tip')
                 .offset([0, 0])
                 .html(function(d) {
-                    return "<span style='color:white'>"+d.date+"</br>"+d.count+" tweets</span>";
+                    return "<span style='color:white'>"+formatDate(d.date)+"</br>"+d.count+" tweets</span>";
                 })
 
             svg.call(tip);
@@ -121,7 +130,74 @@ module.exports = function(config){
             });
 
 
+            // Brushing from http://bl.ocks.org/emeeks/8899a3e8c31d4c5e7cfd
+            // Draw brush
+            brush = d3.svg.brush()
+                .x(xScale)
+                .on("brush", brushmove)
+                .on("brushend", brushend);            
+
+            var arc = d3.svg.arc()
+              .outerRadius(height / 20)
+              .startAngle(0)
+              .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
+
+            brushg = svg.append("g")
+              .attr("class", "brush")
+              .call(brush);
+
+            brushg.selectAll(".resize").append("path")
+                .attr("transform", "translate(0," +  height / 2 + ")")
+                .attr("d", arc);
+
+            brushg.selectAll("rect")
+                .attr("height", height);
+
+            // ****************************************
+            // Brush functions
+            // ****************************************
+
+            function brushmove() {
+                yScale.domain(xScaleIdx.range())
+                        .range(xScaleIdx.domain());
+                b = brush.extent();
+
+                var localBrushYearStart = (brush.empty()) ? brushYearStart : Math.ceil(yScale(b[0])),
+                    localBrushYearEnd = (brush.empty()) ? brushYearEnd : Math.ceil(yScale(b[1]));
+
+                // Snap to rect edge
+                d3.select("g.brush").call((brush.empty()) ? brush.clear() : brush.extent([yScale.invert(localBrushYearStart), yScale.invert(localBrushYearEnd)]));
+
+                // Fade all years in the histogram not within the brush: 
+                // for each bar, if index is within selected range, set opacity to 1
+                // else set opacity to .4
+                d3.selectAll("rect.rect").style("opacity", function(d, i) {
+                  return d.idx >= localBrushYearStart && d.idx < localBrushYearEnd || brush.empty() ? "1" : ".4";
+                });
+
+            }
+
+            function brushend() {
+
+                var localBrushYearStart = (brush.empty()) ? brushYearStart : Math.ceil(yScale(b[0])),
+                  localBrushYearEnd = (brush.empty()) ? brushYearEnd : Math.floor(yScale(b[1]));
+
+                d3.selectAll("rect.bar").style("opacity", function(d, i) {
+                  return d.idx >= localBrushYearStart && d.idx <= localBrushYearEnd || brush.empty() ? "1" : ".4";
+                });
+
+                console.log('local=', [localBrushYearStart,localBrushYearEnd])
+                
+            }
+
+            function resetBrush() {
+              brush
+                .clear()
+                .event(d3.select(".brush"));
+            }
+
         }); //end d3.csv
+
     } //end createTimeline
 
 
